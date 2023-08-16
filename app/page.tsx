48e04a1ae7c5a1aed1a08380350061a252db6580
VurhD <benegaldhruv2312@gmail.com>
2023-08-16 16:32:39 +0800

    Creating the counter on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-
+'use client';
+import { useState } from "react"
 
 export default function Home() {
+  // Creating a counter with useState:
+  const [count, setCount] = useState(0);
+
+  // Function to add a number to the counter:
+
   return (
-      <div>
-        <p className="w-full h-full flex justify-center items-center text-3xl font-extrabold">
-          Hello World
+      <div className="flex flex-col w-full h-full justify-center items-center gap-5">
+        <p className="text-3xl font-extrabold">
+          Count your life away:
         </p>
       </div>
   )
