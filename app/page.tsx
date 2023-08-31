d955c0cb718889d495643f95b2c7b94a06b06c9e
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-08-31 11:50:54 +0800

    Implemented a counter

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,16 @@ export default function Home() {
   const [count, setCount] = useState(0);
 
   // Function to add a number to the counter:
+  const counterFunc = () => {
+    setCount(count + 1);
+  }
 
   return (
-      <div className="flex flex-col w-full h-full justify-center items-center gap-5">
+      <div className="flex flex-col w-full h-full justify-center items-center gap-5 mt-10">
         <p className="text-3xl font-extrabold">
-          Count your life away:
+          Count your life away: {count}
         </p>
+        <button className="button" onClick={counterFunc}>+</button>
       </div>
   )
 }
