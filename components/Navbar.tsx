4c6861256e772584b61f53398b4ac2f08a038543
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-02 10:12:21 +0800

    Added a Github icon and a button for resume download

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,18 @@ const Navbar = (props: Props) => {
     return (
         <div className='flex items-center justify-between w-full h-16 bg-neutral-300 shadow-xl px-5'>
             <h1 className='font-bold text-xl'>My Portfolio</h1>
-            <a href='https://www.linkedin.com/in/dhruv-benegal-60530b220/' target='_blank'>
-                <Icon icon="pajamas:linkedin" className='md-icon' />
-            </a>
+            <div className='flex gap-5 items-center'>
+                <button className='button'>
+                    Resume
+                </button>
+                <a href='https://www.linkedin.com/in/dhruv-benegal-60530b220/' target='_blank'>
+                    <Icon icon="pajamas:linkedin" className='md-icon' />
+                </a>
+                <a href='https://github.com/VurhD-B' target='_blank'>
+                    <Icon icon='bi:github' className='md-icon' />
+                </a>
+            </div>
+            
         </div>
     )
 }
