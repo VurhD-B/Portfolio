3666338e04f033030cc1197a409be10e02c60bd9
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-02 09:59:29 +0800

    Added a Linkedin Icon to link to my linkedin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,8 @@
-'use client';
-import { useState } from "react"
-
 export default function Home() {
-  // Creating a counter with useState:
-  const [count, setCount] = useState(0);
-
-  // Function to add a number to the counter:
-  const counterFuncAdd = () => {
-    setCount(count + 1);
-  }
-
-  // Function to subtract a number from the counter:
-  const counterFuncSubtract = () => {
-    setCount(count - 1);
-  }
-
-  // Function to reset the counter:
-  const counterFuncReset = () => {
-    setCount(0);
-  }
 
   return (
-      <div className="flex flex-col w-full h-full justify-center items-center gap-5 mt-10">
-        <p className="text-3xl font-extrabold">
-          Count your life away: {count}
-        </p>
-        <div className="flex gap-5">
-          <button className="button" onClick={counterFuncAdd}>+</button>
-          <button className="button" onClick={counterFuncSubtract}>-</button>
-          <button className="button" onClick={counterFuncReset}>Reset</button>
-        </div>
+      <div className="">
+        
       </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,16 @@
+'use client'
 import React from 'react'
+import {Icon} from '@iconify/react'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <div className='flex items-center w-full h-16 bg-neutral-300 shadow-xl px-5'>
+        <div className='flex items-center justify-between w-full h-16 bg-neutral-300 shadow-xl px-5'>
             <h1 className='font-bold text-xl'>My Portfolio</h1>
+            <a href='https://www.linkedin.com/in/dhruv-benegal-60530b220/' target='_blank'>
+                <Icon icon="pajamas:linkedin" className='md-icon' />
+            </a>
         </div>
     )
 }
